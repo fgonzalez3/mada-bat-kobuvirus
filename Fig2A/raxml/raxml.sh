2189497384dd9708caf74c35d63636ea57b48087
fgonzalez3 <51669806+fgonzalez3@users.noreply.github.com>
2023-04-26 14:01:26 -0400

    Add files via upload

---
+++ Fig2A/raxml/raxml.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --job-name=picornaraxml
+#SBATCH --nodes=2
+#SBATCH --ntasks-per-node=8
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=1024  
+#SBATCH --time=168:00:00
+#SBATCH --output=picornaraxml.out
+#SBATCH --error=picornaraxml.err
+
+module purge
+module load vim/8.1
+module load emacs/27.2
+module load python/3.9
+module load java/12
+module load cmake/3.20
+module load compiler/gcc/9 
+module load openmpi/4.1 
+module load raxml-ng/1.1
+
+raxml-ng-mpi --all --msa picorna_raxml.fasta --model TVM+I+G4 --prefix T3  --seed 12 --threads 8 --bs-metric fbp,tbe 
---
+++ Fig2A/raxml_check/raxml_check.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --job-name=picornaraxml
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=8
+#SBATCH --time=168:00:00
+#SBATCH --output=picornaraxml.out
+#SBATCH --error=picornaraxml.err
+
+module purge
+module load vim/8.1
+module load emacs/27.2
+module load python/3.9
+module load java/12
+module load cmake/3.20
+module load compiler/gcc/9 
+module load openmpi/4.1 
+module load raxml-ng/1.1
+
+raxml-ng-mpi --check --msa picorna_raxml.fasta --model TVM+I+G4 --prefix T1
