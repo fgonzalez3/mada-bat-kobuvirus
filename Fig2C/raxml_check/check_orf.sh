4ae7f17aac247cc25a5bd04f0d0aa524f43f6950
fgonzalez3 <51669806+fgonzalez3@users.noreply.github.com>
2023-04-26 14:05:07 -0400

    Add files via upload

---
+++ Fig2C/raxml_check/check_orf.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --job-name=orfraxml
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=8
+#SBATCH --time=168:00:00
+#SBATCH --output=orfraxml.out
+#SBATCH --error=orfraxml.err
+
+module purge
+module load vim/8.1
+module load emacs/27.2
+module load python/3.9
+module load java/12
+module load cmake/3.20
+module load compiler/gcc/9 
+module load openmpi/4.1 
+module load raxml-ng/1.1
+
+raxml-ng-mpi --check --msa orf_raxml.fasta --model  LG+G4+F --prefix T1
